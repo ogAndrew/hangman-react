5229d88b8c60589fa818deb9cd87bd755975d1e4
Andrew Talle <talle.andrew@gmail.com>
2020-10-23 15:22:21 -0700

    create static figure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header />
+     <div className="game-container">
+      <Figure />
+     </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+          <h1>Hangman Game</h1>
+          <p>Find the missing letter</p>
+        </>
+    )
+}
+
+export default Header
