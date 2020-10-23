7c7e48f00c7cc0fb3b60039e7458c4037023bf4b
Andrew Talle <talle.andrew@gmail.com>
2020-10-23 16:58:50 -0700

    working game with modal

---
+++ src/components/PopUp.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import {checkWin} from '../helpers/helpers';
+
+const PopUp = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
+    let finalMessage = '';
+    let finalMessageRevealWord = '';
+    let playable = true;
+
+    if (checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
+        finalMessage = 'Congratulations! You won!';
+        playable = false;
+    } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
+        finalMessage = 'Dang dude, you really suck at this!';
+        finalMessageRevealWord = `...the word was ${selectedWord}`;
+        playable = false;
+    }
+
+    useEffect(() => setPlayable(playable));
+
+    return (
+        <div className="popup-container" style={finalMessage !== '' ? {display:'flex'} : {}}>
+            <div className="popup">
+                <h2>{finalMessage}</h2>
+                <h3>{finalMessageRevealWord}</h3>
+                <button>Play Again</button>
+            </div>
+        </div>
+    )
+}
+
+export default PopUp
